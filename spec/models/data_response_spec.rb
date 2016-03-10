2111d52fa9d5f478fe031cbe78e10f34fd70759a
Seachaos <ye.jian.yin@gmail.com>
2016-03-11 01:35:38 +0800

    fix rspec

--- spec/models/data_response_spec.rb
+++ spec/models/data_response_spec.rb
@@ -4,18 +4,14 @@ RSpec.describe DataResponse, type: :model do
 	# pending "add some examples to (or delete) #{__FILE__}"
 
 	describe "DB test." do
-		dataTotal = 0;
-		name = 'TestName'
-
-		before(:each) do
-			dataTotal = 0;
-			name = 'TestName'
-		end
+		let(:name){ 'TestName' }
 
 	 	it "should insert data" do
+	 		dataTotal = 0
 	 		data = DataResponse.new
 	 		data.name = name
-	 		data.save
+	 		data.path = name
+	 		expect(data.save).to be true
 	 		datas = DataResponse.where('name=?', name)
 	 		expect(datas).not_to be_nil
 	 		dataTotal = dataTotal + 1
@@ -23,16 +19,19 @@ RSpec.describe DataResponse, type: :model do
 		end
 
 		it "shoud have datas" do
+			dataTotal = 0
 			datas = DataResponse.where('name=?', name)
 			expect(datas.length).to eq(dataTotal)
-			for i in 0..5 do
+			for i in 1..5 do
 	 			data = DataResponse.new
 		 		data.name = name
-		 		data.save
+		 		data.path = name + i.to_s + 'HD'
+		 		expect(data.save).to be true
 		 		dataTotal = dataTotal + 1
 	 		end
 	 		datas = DataResponse.where('name=?', name)
-	 		expect(datas).not_to be_nil
+	 		expect(datas.present?).to be true
+	 		expect(datas.length).to be >= 1
 	 		expect(datas.length).to eq(dataTotal)
 		end
 	end
